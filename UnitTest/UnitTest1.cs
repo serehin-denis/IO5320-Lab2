9c42747fd61f8d00b13e5d8c93095e17cf1f9a77
serehin-denis <serehindenis@gmail.com>
2026-03-09 21:09:53 +0200

    Initial commit

---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using laba2;
+namespace UnitTest;
+
+public class UnitTest1
+
+{
+    [Fact]
+    public void GetSimilarCountTest()
+    {
+        string testPoem = "Ображайся на мене, як хочеш,\n" +
+                          "Зневажай, ненавидь мене –\n" +
+                          "Все одно я люблю твої очі\n" +
+                          "І волосся твоє сумне.\n\n" +
+                          "Хай досада чи гнів жевріє,\n" +
+                          "Хай до сліз я тебе озлю –\n" +
+                          "Ти для мене не тільки мрія,\n" +
+                          "Я живою себе люблю.";
+        int num = 2;
+        
+        StrOperations poem = new StrOperations(testPoem);
+        
+        Assert.Equal(num, poem.GetSimilarCount());
+    }
+}
---
+++ laba2/StrOperations.cs
@@ -0,0 +1,56 @@
+﻿namespace laba2;
+
+public class StrOperations
+{
+    private string _text = null!;
+
+    private string Data
+    {
+        get => _text;
+        set
+        {
+            if (value == null)
+                throw new ArgumentNullException(nameof(value), "Текст не може бути Null"); 
+            if (value.Length == 0)
+                throw new Exception("Текст не може бути порожнім\n");
+            _text = value;
+        }
+    }
+    
+    
+    public StrOperations(string text)
+    {
+        Data = text;
+    }
+    
+    public override string ToString()
+    {
+        return Data;
+    }
+
+    public int GetSimilarCount()
+    {
+        string[] splittedText = _text.Split(new char[] { '.', '!', '?'}, StringSplitOptions.RemoveEmptyEntries);
+        int counter = 0;
+
+        for (int i = 0; i < splittedText.Length; i++)
+        {
+            string[] words = splittedText[i].Split(new char[] { ' ', ',', '\n', '-'}, StringSplitOptions.RemoveEmptyEntries);
+            
+            HashSet<string> uniqueWords = new HashSet<string>();
+
+            foreach (string word in words)
+            {
+                if (!uniqueWords.Add(word.ToLower()))
+                {
+                    counter++;
+                    break;
+                }
+            }
+            
+        }
+            
+        
+        return counter;
+    }
+}
